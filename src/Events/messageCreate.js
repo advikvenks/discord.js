e5f47be370b74457366ca0c9c33ed8ddbcf63a8a
advikvenks <76761391+advikvenks@users.noreply.github.com>
2022-03-03 10:16:14 +0530

    master

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,36 +1,8 @@
 const Event = require("../structures/Event");
-const capLetters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
 
 module.exports = new Event("messageCreate", (client, message) => {
   if (message.author.bot) return;
-  
+
   if (!message.content.startsWith(client.prefix)) return;
 
   const args = message.content.substring(client.prefix.length).split(/ +/);
