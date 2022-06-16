e98280e3debf174ffcfd0cfc5c43b1ed7024b154
advikvenks <advikvenks09@gmail.com>
2022-06-16 09:49:05 +0530

    master

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const Client = require("./src/structures/Client.js");
 const client = new Client();
 require("dotenv").config();
 
-client.start(process.env.BOT_TOKEN);
+client.start(process.env.TOKEN);
